bb5ee9b0129bfa2d6415dd4cb4401ddf57f546be
chen-assert <jingruichen@gmail.com>
2023-11-05 19:06:36 +0800

    Use pyzipper module to support decrypt AES encrypted file (#9)
    * Use pyzipper module to support decrypt AES encrypted file
    
    * Add support for --use-pyzipper argument
    
    ---------
    
    Co-authored-by: SomeRandom-Dev <freakish@freakish.dev>

--- zz.py
+++ zz.py
@@ -7,12 +7,13 @@
 #           MIT License
 #######################################
 #If you experience any error please let me know
-#version 2.2
+#version 2.3
 
 from colorama import Fore,Back,Style,init
 from itertools import chain,product
 from optparse import OptionParser
 from zipfile import ZipFile
+from pyzipper import AESZipFile
 import string
 import signal
 import time
@@ -39,6 +40,10 @@ class main():
         self.saves_dir = os.path.dirname(os.path.abspath(__file__))+"/stateSaves"
         self.passFound = False
         self.done = False
+        if options.use_pyzipper:
+            self.zip_func = AESZipFile
+        else:
+            self.zip_func = ZipFile
     
     def signal_handler(self, signal, frame):
         self.done=True
@@ -69,7 +74,7 @@ class main():
     def wlist_crack_entry(self, archive_dir, wordlist_inp, showoutput_b, resume_index):
         tries = 0
         try:
-            try: _archive = ZipFile(archive_dir, "r")
+            try: _archive = self.zip_func(archive_dir, "r")
             except Exception as ex: print(ex); sys.exit(1)
 
             print("Loading Wordlist, Please wait")
@@ -132,7 +137,7 @@ class main():
     def bruteforce_crack_entry(self, archive_dir, charset, max_letters, showoutput_b, resume_index):
         tries = 0
         try:
-            try: _archive = ZipFile(archive_dir, "r")
+            try: _archive = self.zip_func(archive_dir, "r")
             except Exception as ex: print(ex); sys.exit(1)
             print("Started...")
             self.bruteforce_crack(tries,None,_archive,showoutput_b, charset, max_letters)
@@ -303,6 +308,8 @@ if __name__ == "__main__":
     parser.add_option("--rl",
                     action="store_true", dest="reslist", default=False,
                     help="Shows a list of all restore files found")
+    parser.add_option("--use-pyzipper", dest="use_pyzipper", default=False,
+                    help="Use pyzipper instead of zipfile (AES)")
     (options, args) = parser.parse_args()
 
     init()    
