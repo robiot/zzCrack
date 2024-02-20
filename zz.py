5243d65e7368d3feae275824ad3bba1cd4b0232d
Bloodlusted <131930548+Bloodlusted@users.noreply.github.com>
2024-02-20 22:18:02 +0800

    Added time taken to crack (#11)

--- zz.py
+++ zz.py
@@ -35,8 +35,11 @@ banner='''
 
 class main():
     def __init__(self):
-        self.passFound_print = lambda tries,word: f"{Style.RESET_ALL}Password was found after {Fore.LIGHTCYAN_EX+str(tries)} {Style.RESET_ALL}tries. The password is: {Fore.LIGHTGREEN_EX+word}"
-        self.tries_print = lambda tries,word: f"Tries: {tries} > {word}"
+        self.passFound_print = lambda tries, word, start_time: (
+        f"{Style.RESET_ALL}Password was found after {Fore.LIGHTCYAN_EX + str(tries)} {Style.RESET_ALL}tries.\n"
+        f"The password is: {Fore.LIGHTGREEN_EX + word}{Style.RESET_ALL}\n"
+        f"Time taken: {Style.BRIGHT + Fore.RED + self.format_time(start_time)}")
+        self.tries_print = lambda tries, word: f"Tries: {tries} > {word}"
         self.saves_dir = os.path.dirname(os.path.abspath(__file__))+"/stateSaves"
         self.passFound = False
         self.done = False
@@ -46,7 +49,25 @@ class main():
             self.zip_func = ZipFile
     
     def signal_handler(self, signal, frame):
-        self.done=True
+        self.done = True
+
+    def format_time(self, start_time):
+        elapsed_time = time.time() - start_time
+        days = int(elapsed_time // (24 * 3600))
+        elapsed_time %= (24 * 3600)
+        hours = int(elapsed_time // 3600)
+        elapsed_time %= 3600
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        time_string = ""
+        if days:
+            time_string += f"{days}d "
+        if hours:
+            time_string += f"{hours}h "
+        if minutes:
+            time_string += f"{minutes}m "
+        time_string += f"{seconds}s"
+        return time_string
 
 
     #############
@@ -54,9 +75,10 @@ class main():
     #############
     def wlist_crack(self, tries, wordlist, z, stream, archive_dir, wordlist_inp):
         content = z.namelist()
+        start_time = time.time()
         for word in wordlist:
             try:
-                if self.done==True:
+                if self.done:
                     confirm = input("\nAre you sure you want to stop (y/n) > ").lower()
                     if confirm == "y": self.save_state(archive_dir, wordlist_inp, word, stream, wordlist); return
                     self.done = False
@@ -66,7 +88,7 @@ class main():
                 z.setpassword(word.encode('utf8', errors='ignore'))
                 z.read(content[0])
                 self.passFound = True
-                print(self.passFound_print(tries,word))
+                print(self.passFound_print(tries,word,start_time))
                 return
             except:
                 pass
@@ -88,7 +110,7 @@ class main():
 
             self.wlist_crack(tries, wordlist, _archive, showoutput_b, archive_dir, wordlist_inp)
         except IOError:
-            print("Archive Or Wordlist Doesen't Exist")
+            print("Archive Or Wordlist Doesn't Exist")
             sys.exit(1)
 
 
@@ -109,15 +131,15 @@ class main():
         if charlist == "": print("Invalid charset"); sys.exit(1)
         return charlist
 
-    def memsafe_generate_bruteforce_list(self, charset, max_lenght):
+    def memsafe_generate_bruteforce_list(self, charset, max_length):
         charlist = self.generate_charlist(charset)
         return (''.join(candidate)
             for candidate in chain.from_iterable(product(charlist, repeat=i)
-            for i in range(1, max_lenght + 1)))
+            for i in range(1, max_length + 1)))
             
-    def bruteforce_crack(self, tries, brutelist, z, stream, charset, max_lenght):
+    def bruteforce_crack(self, tries, brutelist, z, stream, charset, max_length):
         content = z.namelist()
-        for word in self.memsafe_generate_bruteforce_list(charset, max_lenght):
+        for word in self.memsafe_generate_bruteforce_list(charset, max_length):
             try:
                 if self.done == True:
                     confirm = input("\nAre you sure you want to stop (y/n) > ").lower()
@@ -142,7 +164,7 @@ class main():
             print("Started...")
             self.bruteforce_crack(tries,None,_archive,showoutput_b, charset, max_letters)
         except IOError:
-            print("Zipfile Doesent Exist")
+            print("Zipfile Doesn't Exist")
             sys.exit(1)
 
 
@@ -207,14 +229,14 @@ class main():
             self.wlist_crack_entry(archive_dir, wordlist_inp, showoutput_b, None)
 
         elif attack_type == "0":
-            charset = input('\n[+] Which characters to use in the attack (combine to use multiple, separated by +)\n 0: Only small letters\n 1: Only big letters\n 2: Only numbers\n 3: Only signs\n 4: Everything\n> ')
+            charset = input('\n[+] Which characters to use in the attack (combine to use multiple, separated by +)\n 0: Only small letters\n 1: Only big letters\n 2: Only numbers\n 3: Only symbols\n 4: Everything\n> ')
             try:
                 max_letters = int(input("\n[-+] Max length > "))
                 if max_letters < 0:
-                    print("The given number is not a vaild Positive number")
+                    print("The given number is not a valid Positive number")
                     sys.exit(1)
             except:
-                print("The input given is not a vaild Integer number")
+                print("The input given is not a valid Integer")
                 sys.exit(1)
             showOutPut = input("\nDo you want to output everything (slower) or not (faster)  y/n > ")
             if showOutPut.lower() == "y": showoutput_b = True
@@ -242,16 +264,16 @@ class main():
                 print("No Charlist Given")
                 sys.exit(1)
 
-            max_letters_inp = options.maxlenght
+            max_letters_inp = options.maxlength
             if max_letters_inp == None:
-                print("No MaxLenght Given")
+                print("No MaxLength Given")
             try:
                 max_letters = int(max_letters_inp)
                 if max_letters < 0:
-                    print("The given MaxLenght is not a vaild Positive number")
+                    print("The given MaxLength is not a valid Positive number")
                     sys.exit(1)
             except:
-                print("The given MaxLenght is not a vaild Integer number")
+                print("The given MaxLength is not a valid Integer")
                 sys.exit(1)
 
             showoutput_b = options.showoutput
@@ -283,7 +305,7 @@ class main():
         print(Style.RESET_ALL)
     
         if self.passFound == False:
-            print("Couldnt Crack The Password With The Current Options")
+            print("Couldn't Crack The Password With The Current Options")
         
 
 if __name__ == "__main__":
@@ -297,14 +319,14 @@ if __name__ == "__main__":
                     help="Attack method to bruteforce the zipfile password")              
     parser.add_option("-c", dest="charlist",
                     help="(Required for Bruteforce)\nWhich characters to use in the Bruteforce attack (combine to use multiple, separated by +) 0: Only small letters | 1: Only big letters | 2: Only numbers | 3: Only signs | 4: Everything", metavar=" ") #byt
-    parser.add_option("-l", dest="maxlenght",
-                    help="The max lenght for the bruteforce passwords (Required for Bruteforce)", metavar=" ")
+    parser.add_option("-l", dest="maxlength",
+                    help="The max length for the bruteforce passwords (Required for Bruteforce)", metavar=" ")
     parser.add_option("-s", "--stream",
                     action="store_true", dest="showoutput", default=False,
                     help="Stream tried passwords (slower)")
     #restore
     parser.add_option("-r", "--restore", dest="restore",
-                    help="Restore state From save (do --rl for a list of vaild options)", metavar=" ")
+                    help="Restore state From save (do --rl for a list of valid options)", metavar=" ")
     parser.add_option("--rl",
                     action="store_true", dest="reslist", default=False,
                     help="Shows a list of all restore files found")
